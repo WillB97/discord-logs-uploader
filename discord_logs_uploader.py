3c703114ff634b04e68d5fab73cce48fa57eea8b
WillB97 <will@rudi.org.uk>
2021-01-26 18:50:03 +0000

    Initial log uploader Single command for import and upload through discord Only handles logs, no animations

---
+++ discord_logs_uploader.py
@@ -0,0 +1,172 @@
+#!/usr/bin/env python3
+import os
+import re
+import sys
+import logging
+import tempfile
+from pathlib import Path
+from zipfile import ZipFile, BadZipFile, is_zipfile
+
+import discord
+from dotenv import load_dotenv
+from discord.ext import commands
+
+# name of the role able to execute the command
+ADMIN_ROLE = 'Blue Shirt'
+
+# prefix of team channels
+TEAM_PREFIX = 'team-'
+
+DOWNLOADS_DIRECTORY = Path(__file__).parent / 'downloads'
+
+logger = logging.getLogger('logs_uploader')
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.INFO)
+logger.addHandler(handler)
+
+bot = commands.Bot(command_prefix='!')
+
+
+async def log_and_reply(ctx: commands.Context, error_str: str) -> None:
+    logger.error(error_str)
+    await ctx.reply(error_str)
+
+
+async def logs_upload(ctx: commands.Context, file: Path, event_name: str) -> None:
+    try:
+        with tempfile.TemporaryDirectory() as tmpdir_name:
+            tmpdir = Path(tmpdir_name)
+            completed_tlas = []
+
+            with ZipFile(file) as zipfile:
+                for archive_name in zipfile.namelist():
+                    if not archive_name.lower().endswith('.zip'):  # skip non-zips
+                        logger.debug(f"{archive_name} from {file.name} is not a ZIP, skipping")
+                        continue
+
+                    # skip files not starting with TEAM_PREFIX
+                    if not archive_name.lower().startswith(TEAM_PREFIX):
+                        logger.debug(
+                            f"{archive_name} from {file.name} "
+                            f"doesn't start with {TEAM_PREFIX}, skipping",
+                        )
+                        continue
+
+                    zipfile.extract(archive_name, path=tmpdir)
+
+                    if not is_zipfile(tmpdir / archive_name):  # test file is a valid zip
+                        await log_and_reply(
+                            ctx,
+                            f"# {archive_name} from {file.name} is not a valid ZIP file",
+                        )
+                        # The file will be removed with the temporary directory
+                        continue
+
+                    # extract team name from filename
+                    tla_search = re.match(TEAM_PREFIX + r'(.*?)[-.]', archive_name)
+                    if not isinstance(tla_search, re.Match):
+                        await log_and_reply(
+                            ctx,
+                            f"# Failed to extract a TLA from {archive_name} in {file.name}",
+                        )
+                        continue
+
+                    tla = tla_search.group(1)
+
+                    # get team's channel
+                    channel = discord.utils.get(
+                        ctx.guild.channels,
+                        name=f"{TEAM_PREFIX}{tla}",
+                    )
+                    if not channel:
+                        await log_and_reply(
+                            ctx,
+                            f"# Channel {TEAM_PREFIX}{tla} not found, "
+                            f"unable to upload {archive_name}",
+                        )
+                        continue
+                    elif not isinstance(channel, discord.TextChannel):
+                        await log_and_reply(
+                            ctx,
+                            f"# {channel.name} is not a text channel, unable to send message",
+                        )
+                        continue
+
+                    # upload to team channel with message
+                    try:
+                        await channel.send(
+                            content=(
+                                "Here are your logs from "
+                                f"{event_name if event_name else 'today'}"
+                            ),
+                            file=discord.File(str(tmpdir / archive_name)),
+                        )
+                        logger.debug(
+                            f"Uploaded logs for {tla} from "
+                            f"{event_name if event_name else 'today'}",
+                        )
+                    except discord.HTTPException as e:  # handle file size issues
+                        if e.status == 413:
+                            file_size = (tmpdir / archive_name).stat().st_size
+                            await log_and_reply(
+                                ctx,
+                                f"# {archive_name} was too large to upload at "
+                                f"{file_size / 1000**2 :.3f} MiB",
+                            )
+                            continue
+                        else:
+                            raise e
+
+                    completed_tlas.append(tla)
+
+            await ctx.reply(
+                f"Successfully uploaded logs to {len(completed_tlas)} teams: "
+                f"{', '.join(completed_tlas)}",
+            )
+    except BadZipFile:
+        await log_and_reply(ctx, f"# {file.name} is not a valid ZIP file")
+    finally:
+        file.unlink()  # delete the zip file
+
+
+@bot.event
+async def on_ready() -> None:
+    logger.info(f"{bot.user} has connected to Discord!")
+
+
+@bot.command()
+# @commands.guild_only()
+@commands.check_any(commands.has_role(ADMIN_ROLE), commands.is_owner())
+async def logs_import(ctx: commands.Context, event_name: str = "") -> None:
+    """
+    Send a combined logs archive to the bot for distribution to teams
+    - event_name: Optionally set the event name used in the bot's message to teams
+    """
+    logger.info(f"{ctx.author} ran '{ctx.message.content}' on {ctx.guild}:{ctx.channel}")
+
+    for file in ctx.message.attachments:
+        logger.debug(
+            f'Files received {file.filename}: {file.size/1024**2}MB, {file.size/1000**2}MiB',
+        )
+
+    if (
+        ctx.message.attachments
+        and ctx.message.attachments[0].filename.lower().endswith('.zip')
+    ):
+        attachment = ctx.message.attachments[0]
+        save_file = DOWNLOADS_DIRECTORY / attachment.filename
+
+        with save_file.open('wb') as zipfile:
+            await attachment.save(zipfile)
+
+        await logs_upload(ctx, save_file, event_name)
+    else:
+        logger.error(
+            f"ZIP file not attached to '{ctx.message.content}' from {ctx.author}",
+        )
+        await ctx.reply("This command requires the logs archive to be attached")
+
+
+load_dotenv()
+bot.run(os.getenv('DISCORD_TOKEN'))
